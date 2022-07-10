fa1efc80f945cddfc25b20513055887d28ee5fe9
jebnicholson <26765018+jeb5@users.noreply.github.com>
2022-07-11 00:19:29 +1200

    Fixed yet another mobile safari bug (preventing bottom toolbar from occluding popup)

--- client/src/Popup.tsx
+++ client/src/Popup.tsx
@@ -4,14 +4,17 @@ import "./Popup.css";
 
 export default function Popup(props: { title: string; closeAction: () => void; children?: React.ReactNode }) {
 	return (
-		<div className="popup-overlay" onClick={props.closeAction}>
-			<div className="popup" onClick={e => e.stopPropagation()}>
-				<div className="popup-header-bar">
-					<h2>{props.title}</h2>
-					<Icon iconName="exit" onClick={props.closeAction} />
+		<>
+			<div className="popup-overlay"></div>
+			<div className="popup-positioner" onClick={props.closeAction}>
+				<div className="popup" onClick={e => e.stopPropagation()}>
+					<div className="popup-header-bar">
+						<h2>{props.title}</h2>
+						<Icon iconName="exit" onClick={props.closeAction} />
+					</div>
+					<div className="popup-content">{props.children}</div>
 				</div>
-				<div className="popup-content">{props.children}</div>
 			</div>
-		</div>
+		</>
 	);
 }
